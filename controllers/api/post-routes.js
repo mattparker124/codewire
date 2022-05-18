fc8e0eda09df13d786b64dfe9c432f24134b50f0
Matthew Parker <mattparker124@gmail.com>
2022-05-17 19:48:28 -0500

    added post model and get route

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,29 @@
+const sequelize = require('../../config/connection');
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// GET all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
