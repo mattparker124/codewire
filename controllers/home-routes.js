61cc0c3b93883c1c2fc586cf77f43c517f96d7bf
thetryworks <natesmyth1@gmail.com>
2022-05-19 16:27:51 -0500

    got the logout and login buttons to conditionally render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,7 +5,9 @@ const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
   
-  res.render('homepage');
+  res.render('homepage', {
+    loggedIn: req.session.loggedIn
+  });
 });
 
 router.get('/login', (req, res) => {
