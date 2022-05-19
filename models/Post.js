ac91253c5674d0054bbcb5c9f9e153c20330e731
Matthew Parker <mattparker124@gmail.com>
2022-05-18 20:40:13 -0500

    add vote model and associations

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'title',
+                    'tag',
+                    'post_body',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            })
+        })
+    }
+}
 
 Post.init(
     {
