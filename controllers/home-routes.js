a685fedf44cf95423ba48d203a0193110656c7b7
thetryworks <natesmyth1@gmail.com>
2022-05-19 15:02:29 -0500

    fully established user creation and session storage. Ability to log and out is functional

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,21 +2,24 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 
+
 router.get('/', (req, res) => {
+  
   res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
+  res.render('login');
+});
+  
 router.get('/signup', (req, res) => {
-  res.render('signup');
-})
+  
+    res.render('signup');
+});
 
 module.exports = router;
