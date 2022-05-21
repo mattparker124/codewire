e8c94630aab033095262fe634ad030bfa5781d65
Gart <gmhobson@gmail.com>
2022-05-20 19:55:52 -0500

    posts are rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,43 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-
+const { Post, User, Comment } = require('../models')
 
 
 router.get('/', (req, res) => {
-  
-  res.render('homepage', {
-    loggedIn: req.session.loggedIn
-  });
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'tag',
+      'post_body',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      //get full list of posts
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass a single post object into the homepage template
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/login', (req, res) => {
