995254a8bbef7f71278bf643b525962e3cb2d096
thetryworks <natesmyth1@gmail.com>
2022-05-22 10:44:42 -0500

    able to select a tag and create a post

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,12 +2,15 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
+    const tag = document.querySelector('select[name="tag-choice"]').value;
     const post_body = document.querySelector('textarea[name="post-body"]').value;
+    
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
         title,
+        tag,
         post_body
       }),
       headers: {
