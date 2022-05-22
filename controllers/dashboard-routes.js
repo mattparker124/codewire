365fd3cba63d00075c36e75926bbce77ccd35560
thetryworks <natesmyth1@gmail.com>
2022-05-22 11:00:42 -0500

    dashboard now displays only the logged in users posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,48 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
-  });
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'tag',
+      'post_body',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      //get full list of posts
+      const rawPosts = dbPostData.map(post => post.get({ plain: true }));
+      //reverse the posts, so that the newest posts display first
+      const posts = rawPosts.reverse();
+      //render the homepage
+      res.render('dashboard', { 
+        posts,
+        loggedIn: true 
+       });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
