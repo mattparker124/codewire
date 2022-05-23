757692183dfba2d49516ceb81fc1cc4056ac2aae
Gart <gmhobson@gmail.com>
2022-05-23 15:41:10 -0500

    fixed single-post bug

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -83,7 +83,7 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
+      res.render('single-post', { post, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
       console.log(err);
