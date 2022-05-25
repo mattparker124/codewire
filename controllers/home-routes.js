b3743a8851159db9df3faea9aec4e123233fc503
Gart <gmhobson@gmail.com>
2022-05-25 05:38:14 -0500

    edit functionality and universal homepage access

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -105,4 +105,36 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.get('/edit/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'tag',
+      'post_body',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('edit', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,28 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+    
+
+    const title = document.querySelector('textarea[name="post-title"]').value;
+    const post_body = document.querySelector('textarea[name="post-body"]').value;
+    const url = window.location.href.split("")
+    const id = url[(url.length-1)]
+
+    console.log(id)
+
+    const response = await fetch(`/api/posts/`+id, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            post_body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (response.ok) {
+            document.location.replace('/post/'+id)
+        }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
